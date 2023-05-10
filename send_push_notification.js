e40274e2307aeebb4d6d67bcf8da2b9d78bac8cd
Jeeven Lamichhane <lamichhaneaj@gmail.com>
2023-05-10 13:09:21 +0545

    updates: file changes

--- send_push_notification.js
+++ send_push_notification.js
@@ -1,11 +1,11 @@
-const admin = require('firebase-admin');
-const fs = require('fs');
-const serviceAccount = require('./nepali-pulse-firebase-adminsdk-ty2da-303e4efd51.json'); // Path to your service account JSON file
+const admin = require("firebase-admin");
+const fs = require("fs");
+
+const serviceAccount = require("/home/superx/Documents/nepali-pulse-firebase-adminsdk-ty2da-303e4efd51.json"); // Path to your service account JSON file
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
 
 // Get command-line arguments passed to the script
 const args = process.argv.slice(2);
@@ -13,43 +13,44 @@ const args = process.argv.slice(2);
 // Parse and process the command-line arguments
 const data = {};
 args.forEach((arg) => {
-  const [key, value] = arg.split('=');
+  const [key, value] = arg.split("=");
   data[key] = value;
 });
-
-
 
 // Function to send push notification
 function sendPushNotification() {
   // Prepare the notification payload
   const payload = {
     notification: {
-      title: 'Data Change Notification',
-      body: `The ${data['name']} file has been modified.`,
+      title: "Data Change Notification",
+      body: `The ${data["name"]} file has been modified.`,
     },
   };
 
- // Specify the topic to which the notification should be sent
- const topic = 'announcement';
+  // Specify the topic to which the notification should be sent
+  const topic = "announcement";
 
- // Send the notification to the topic
- admin.messaging().sendToTopic(topic, payload)
- .then((response) => {
-   const logMessage = `Push notification sent successfully: ${JSON.stringify(response)}`;
-   console.log(logMessage);
+  // Send the notification to the topic
+  admin
+    .messaging()
+    .sendToTopic(topic, payload)
+    .then((response) => {
+      const logMessage = `Push notification sent successfully: ${JSON.stringify(
+        response
+      )}`;
+      console.log(logMessage);
 
-   // Write the log message to a file
-   fs.appendFileSync('notification.log', logMessage + '\n');
- })
- .catch((error) => {
-   const errorMessage = `Error sending push notification: ${error}`;
+      // Write the log message to a file
+      fs.appendFileSync("notification.log", logMessage + "\n");
+    })
+    .catch((error) => {
+      const errorMessage = `Error sending push notification: ${error}`;
 
-   console.error(errorMessage);
+      console.error(errorMessage);
 
-   // Write the error message to a file
-   fs.appendFileSync('notification.log', errorMessage + '\n');
- });
+      // Write the error message to a file
+      fs.appendFileSync("notification.log", errorMessage + "\n");
+    });
 }
-
 
 sendPushNotification();
