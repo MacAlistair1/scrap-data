ae20630efb5f95b51394abaa1ffb0944d8e38df1
MacAlistair1 <lamichhaneaj@gmail.com>
2024-12-19 13:33:30 +0545

    Automatic commit

--- send_push_notification.js
+++ send_push_notification.js
@@ -1,20 +1,14 @@
-const admin = require("firebase-admin");
+const https = require("https");
+const { google } = require("googleapis");
 const fs = require("fs");
-const https = require('https');
 
-const serviceAccount = require("/Users/nepalivlog/Documents/nepali-pulse-3586c217f2cd.json"); 
+const serviceAccount = require("/Users/nepalivlog/Documents/nepali-pulse-3586c217f2cd.json");
 
-const PROJECT_ID = '<YOUR-PROJECT-ID>';
-const HOST = 'fcm.googleapis.com';
-const PATH = '/v1/projects/' + PROJECT_ID + '/messages:send';
-const MESSAGING_SCOPE = 'https://www.googleapis.com/auth/firebase.messaging';
+const PROJECT_ID = "nepali-pulse";
+const HOST = "fcm.googleapis.com";
+const PATH = "/v1/projects/" + PROJECT_ID + "/messages:send";
+const MESSAGING_SCOPE = "https://www.googleapis.com/auth/firebase.messaging";
 const SCOPES = [MESSAGING_SCOPE];
-
-
-
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-});
 
 // Get command-line arguments passed to the script
 const args = process.argv.slice(2);
@@ -28,51 +22,36 @@ args.forEach((arg) => {
 
 // Function to send push notification
 function sendPushNotification() {
-
-  getAccessToken().then(function(accessToken) {
+  getAccessToken().then(function (accessToken) {
     const options = {
       hostname: HOST,
       path: PATH,
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Authorization': 'Bearer ' + accessToken
-      }
+        Authorization: "Bearer " + accessToken,
+      },
     };
 
+    const request = https.request(options, function (resp) {
+      resp.setEncoding("utf8");
+      resp.on("data", function (data) {
+        const logMessage = `Push notification sent successfully for ${
+          data["name"]
+        }: ${JSON.stringify(data)}`;
+        fs.appendFileSync("notification.log", logMessage + "\n");
+      });
+    });
 
-
-  // Prepare the notification payload
-  const payload = {
-    notification: {
-      title: data["title"] ?? "Data Change Notification",
-      body: data["message"] ?? "Click to Check out the latest update!!",
-    },
-  };
-
-  // Specify the topic to which the notification should be sent
-  const topic = "announcement";
-
-  // Send the notification to the topic
-  admin
-    .messaging()
-    .sendToTopic(topic, payload)
-    .then((response) => {
-      const logMessage = `Push notification sent successfully for ${
-        data["name"]
-      }: ${JSON.stringify(response)}`;
-      // console.log(logMessage);
-
-      // Write the log message to a file
-      fs.appendFileSync("notification.log", logMessage + "\n");
-    })
-    .catch((error) => {
-      const errorMessage = `Error sending push notification for ${data["name"]}: ${error}`;
-
-      // console.error(errorMessage);
-
-      // Write the error message to a file
+    request.on("error", function (err) {
+      const errorMessage = `Error sending push notification for ${data["name"]}: ${err}`;
       fs.appendFileSync("notification.log", errorMessage + "\n");
     });
+
+    const commonMessage = buildCommonMessage();
+
+    request.write(JSON.stringify(commonMessage));
+    request.end();
+  });
 }
 
 function getAccessToken() {
@@ -93,6 +72,18 @@ function getAccessToken() {
       resolve(tokens.access_token);
     });
   });
+}
+
+function buildCommonMessage() {
+  return {
+    message: {
+      topic: "announcement",
+      notification: {
+        title: data["title"] ?? "Data Change Notification",
+        body: data["message"] ?? "Click to Check out the latest update!!",
+      },
+    },
+  };
 }
 
 sendPushNotification();
